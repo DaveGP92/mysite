f95a6350b403dbb3fb7d726f82730b2d465d318d
Dave <dave.gonzalez1992@outlook.com>
2024-10-22 21:33:24 -0500

    Creación de paginación

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 from .models import Post
 
 
 def posts_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(request, 'blog/post/list.html', {
         'posts': posts
     })
 
+ 
 def post_detail(request, year, month, day, post):
 
     post = get_object_or_404(Post, status= Post.Status.PUBLISHED,
